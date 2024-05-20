3ffacf174145f58431532baa913df001ee715419
sloopee1 <140573738+sloopee1@users.noreply.github.com>
2024-05-20 23:35:22 +1000

    Update #SDD 2024.py

--- #SDD 2024.py
+++ #SDD 2024.py
@@ -1,1 +1,15 @@
 #SDD 2024
+import tkinter as tk 
+import customtkinter as ctk
+import random
+
+#Set up appearance and colour theme
+ctk.set_appearance_mode("Dark") #Options are: System, Light, Dark
+ctk.set_default_color_theme("blue") #Options are: Blue, Green, Dark Blue
+
+class MultiplicationApp(ctk.CTk):
+    def __innit__(self_)
+        super().__innit__()
+        self.title("Multiplication Learning App")
+        self.geometry("800x800")
+        
