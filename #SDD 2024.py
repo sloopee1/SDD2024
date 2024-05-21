c008ac3d2ccbfa25ac6ef18f2a1feb8fad2e47dc
Hamdan Elomar <hamdan@Hamdans-MacBook-Pro.local>
2024-05-21 22:18:10 +1000

    Update #SDD 2024.py

--- #SDD 2024.py
+++ #SDD 2024.py
@@ -21,6 +21,24 @@ class MultiplicationApp(ctk.CTk):
 
         self.dark_mode = tk.BooleanVar(value=True) #Keeping track of whether it is in dark mode or not
 
+        #Main Menu Frame Setup
+        self.menu_frame = ctk.CTkFrame(self, width=720, height=50, corner_radius=10)
+        self.menu_frame.place(relx=0.5, rely=0.1, anchor=tk.N)
+        \
+        #Create Buttons For Lesson, Quiz, and Settings
+        self.lesson_button = ctk.CTkButton(self.menu_frame, text="Lesson", command=self.show_lesson)
+        self.quiz_button = ctk.CTkButton(self.menu_frame, text="Quiz", command=self.show_quiz)
+        self.settings_button = ctk.CTkButton(self.menu_frame, text="Settings", command=self.show_settings)
+
+        #Initialize frames for different parts of the app
+        self.lesson_frame = self.create_lesson_frame()
+        self.quiz_frame = self.create_quiz_frame()
+        self.settings_frame = self.create_settings_frame()
+
+        #Display the lesson frame initally
+        self.show_lesson()
         
+
+
 
         
